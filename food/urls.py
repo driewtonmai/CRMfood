48deb7ca5d527932d7bc75a849b50ab3ef73b370
driewtonmai <bigbluelamp@gmail.com>
2019-12-02 23:54:29 +0600

    the commit will add generics.RetrieveUpdateDestroy to 'Users'

--- food/urls.py
+++ food/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('departments/', DepartmentsCreateView.as_view(), name='department_create'),
     path('departments/<int:pk>/', DepartmentsRetrieveView.as_view(), name='departments_retrieve'),
     path('users/', UserCreateView.as_view(), name='user_create'),
+    path('users/<int:pk>/', UserRetrieveView.as_view(), name='user_retrieve'),
     path('mealcategories/', MealCategoriesCreateView.as_view(), name='mealcategories_create'),
     path('mealcategories/<int:pk>/', MealCategoriesRetrieveView.as_view(), name='mealcategories_retrieve'),
     path('meals/', MealsCreateView.as_view(), name='meals_create'),
